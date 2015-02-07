13527d9cbcf19b4ea6a56805f823b3f0cb0d3fe2
George <vitalijkozacek@MacBook-Air-George.local>
2015-02-07 10:26:53 +0200

    Initial Commit

---
+++ vkMusiс/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  vkMusiс
+//
+//  Created by George on 07.02.15.
+//  Copyright (c) 2015 George. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
